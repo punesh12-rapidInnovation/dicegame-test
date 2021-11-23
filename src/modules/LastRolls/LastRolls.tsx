f1240750e88c555fa532aafb215a55d792c21b28
webdevwaseem <waseemwebdev81@gmail.com>
2021-11-23 15:13:47 +0530

    Last rolls ui done

---
+++ src/modules/LastRolls/LastRolls.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Box, BoxTitle, TR, TD } from './style';
+import { PrimaryButton } from 'shared/button/Button';
+import Diceicon from '../../assets/icons/Diceicon.svg';
+
+function LastRolls() {
+    return (
+        <Box style={{width: '45%',maxWidth:'700px',height:'400px',marginRight:'20px',marginTop:'30px',padding:"20px",background:"#2A1966",display:'flex',justifyContent:'space-between',alignItems:'center',flexDirection:'column'}}>
+            <BoxTitle>YOUR LAST ROLLS <img src={Diceicon} alt="" style={{ marginLeft: '10px' }}/></BoxTitle>
+                        <div style={{width: '100%', boxShadow: "inset 0px 0px 24px #ca1ae733", borderRadius: "20px",color:"#fff",padding:"20px 30px",height:'250px',marginTop:'40px',paddingBottom:'40px'}}>
+                            <table style={{width:"100%"}}>
+                                <TR style={{height:'40px'}}>
+                                    <TD style={{textAlign:'left'}}># DATE AND TIME</TD>
+                                    <TD>BET AMOUNT</TD>
+                                    <TD>MIN CHANCE</TD>
+                                    <TD>GAIN/LOSS</TD>
+                                </TR>
+                                <TR>
+                                    <TD style={{textAlign:'left'}}>#1 - 23|Oct|2022 - 19:11</TD>
+                                    <TD>23.36 PLS</TD>
+                                    <TD>26%</TD>
+                                    <TD>40 PLS</TD>
+                                    
+                                </TR>
+                                <TR>
+                                    <TD style={{textAlign:'left'}}>#1 - 23|Oct|2022 - 19:11</TD>
+                                    <TD>23.36 PLS</TD>
+                                    <TD>26%</TD>
+                                    <TD>40 PLS</TD>
+                                    
+                                </TR>
+                                 <TR>
+                                    <TD style={{textAlign:'left'}}>#1 - 23|Oct|2022 - 19:11</TD>
+                                    <TD>23.36 PLS</TD>
+                                    <TD>26%</TD>
+                                    <TD>40 PLS</TD>
+                                    
+                                </TR>
+                                <TR>
+                                    <TD style={{textAlign:'left'}}>#1 - 23|Oct|2022 - 19:11</TD>
+                                    <TD>23.36 PLS</TD>
+                                    <TD>26%</TD>
+                                    <TD>40 PLS</TD>
+                                    
+                                </TR>
+                            </table>
+                        </div>
+                        <PrimaryButton style={{width:'50%',transform:'translatey(-25px)'}}>SHOW MORE</PrimaryButton>
+        </Box>
+    )
+}
+
+export default LastRolls
---
+++ src/modules/LastRolls/style.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+
+export const Box = styled.div`
+background: linear-gradient(90deg, rgba(239, 8, 150, 0.1) -6.9%, rgba(112, 7, 255, 0.1) 55.31%, rgba(0, 200, 255, 0.1) 107.28%);
+box-shadow: 0px 3px 5px rgba(66, 20, 74, 0.6), inset 0px 0px 20px rgba(202, 26, 231, 0.9);
+border-radius: 20px;
+*{
+
+
+}
+`
+
+export const BoxTitle = styled.div`
+color: #fff;
+font-size: 20px;
+font-weight: 600;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 10px;
+`
+
+export const TD = styled.td`
+font-size: 13px;
+font-weight: 500;
+text-align: center;
+`
+
+export const TR = styled.tr`
+height: 35px;
+
+`
+
