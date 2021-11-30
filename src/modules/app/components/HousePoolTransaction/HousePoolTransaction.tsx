d0388a6afbb82c3bb7c49c488bc8a41cdb3a0955
punesh12-rapidInnovation <punesh@rapidinnovation.dev>
2021-11-30 16:05:17 +0530

    add housepool transactions

---
+++ src/modules/app/components/HousePoolTransaction/HousePoolTransaction.tsx
@@ -0,0 +1,180 @@
+import React, { useState } from 'react';
+import { usePagination, useTable } from 'react-table';
+import { DataContainer, TableStyles } from './style';
+
+
+const HousePoolTransaction = () => {
+
+
+    const [TokenData, setTokenData] = useState<any>([
+        {
+            'action': "Deposit",
+            'total_value': 256.22,
+            'account': '0x5f0da096A0B4e9da',
+            'time': '23|Oct|2022 - 19:11',
+        },
+        {
+            'action': "Deposit",
+            'total_value': 256.22,
+            'account': '0x5f0da096A0B4e9da',
+            'time': '23|Oct|2022 - 19:11',
+        },
+        {
+            'action': "Deposit",
+            'total_value': 256.22,
+            'account': '0x5f0da096A0B4e9da',
+            'time': '23|Oct|2022 - 19:11',
+        },
+        {
+            'action': "Deposit",
+            'total_value': 256.22,
+            'account': '0x5f0da096A0B4e9da',
+            'time': '23|Oct|2022 - 19:11',
+        },
+    ])
+
+
+    function Table({ columns, data }: { columns: any, data: any }) {
+        // Use the state and functions returned from useTable to build your UI
+        const {
+            getTableProps,
+            getTableBodyProps,
+            headerGroups,
+            prepareRow,
+            // @ts-ignore
+            page, // Instead of using 'rows', we'll use page,
+            // which has only the rows for the active page
+
+            // The rest of these things are super handy, too ;)
+            // @ts-ignore
+            canPreviousPage,
+            // @ts-ignore
+            canNextPage,
+            // @ts-ignore
+            pageOptions,
+            // @ts-ignore
+            pageCount,
+            // @ts-ignore
+            gotoPage,
+            // @ts-ignore
+            nextPage,
+            // @ts-ignore
+            previousPage,
+            // @ts-ignore
+            setPageSize,
+            // @ts-ignore
+            state: {
+                // @ts-ignore
+                pageIndex,
+                // @ts-ignore
+                pageSize
+            },
+        } = useTable(
+            {
+                columns,
+                data,
+                initialState: {
+                    // @ts-ignore 
+                    pageIndex: 0
+                },
+            },
+            usePagination
+        )
+
+        // Render the UI for your table
+        return (
+            <>
+                <table {...getTableProps()}>
+                    <thead>
+                        {headerGroups.map((headerGroup: any) => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map((column: any) => (
+                                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+
+                    <tbody {...getTableBodyProps()}>
+                        {
+                            page.length ?
+                                page.map((row: any, i: number) => {
+                                    prepareRow(row)
+
+                                    return (
+                                        <tr {...row.getRowProps()}>
+                                            {row.cells.map((cell: any) => {
+
+                                                // else
+                                                // if (cell.column.id === 'dollar_Price') return <td {...cell.getCellProps()}>$ {cell.value}</td>
+                                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                            })}
+                                        </tr>
+                                    )
+                                })
+                                :
+                                <tr>
+                                    <td colSpan={5} style={{ textAlign: "center" }}>No Data Available</td>
+                                </tr>
+                        }
+                    </tbody>
+                </table>
+                {/* 
+          Pagination can be built however you'd like. 
+          This is just a very basic UI implementation:
+        */}
+                {
+                    page.length ?
+                        <div className="pagination">
+                            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                                {'<'}
+                            </button>{' '}
+                            <strong>
+                                Page {pageIndex + 1} of {pageOptions.length}
+                            </strong>
+                            <button onClick={() => nextPage()} disabled={!canNextPage}>
+                                {'>'}
+                            </button>{' '}
+                            {' '}
+                        </div>
+                        : null
+                }
+            </>
+        )
+    }
+
+
+    const columns = React.useMemo(
+        () => [
+
+            {
+                Header: 'ACTION',
+                accessor: 'action',
+            },
+            {
+                Header: 'TOTAL VALUE',
+                accessor: 'total_value',
+            },
+            {
+                Header: 'ACCOUNT',
+                accessor: 'account',
+            },
+            {
+                Header: 'TIME',
+                accessor: 'time',
+            },
+        ],
+        []
+    )
+    return (
+        <DataContainer>
+            HousePoolTransaction
+            <TableStyles  >
+
+                {TokenData && <Table columns={columns} data={TokenData} />}
+            </TableStyles>
+        </DataContainer>
+    );
+};
+
+export default HousePoolTransaction;
--- src/pages/housePool/HousePool.tsx
+++ src/pages/housePool/HousePool.tsx
@@ -3,11 +3,14 @@ import React, { useState } from 'react';
 import { PrimaryButton } from 'shared/button/Button';
 import CustomModal from 'shared/custom-modal';
 import { colors } from 'shared/styles/theme';
-import { InfoContainer, HousePoolCont, H1, FlexCont, H3, Link, InfoFlexCont, PoolDetailsContainer, PoolDetails, PoolFundsCont } from './style';
+import { InfoContainer, HousePoolCont, H1, FlexCont, H3, Link, InfoFlexCont, PoolDetailsContainer, PoolDetails, PoolFundsCont, TransactionContainer } from './style';
 import verticalLine from "assets/icons/verticalLine.svg";
+import Disclaimer from 'shared/Disclaimer/Disclaimer';
+import HousePoolTransaction from 'modules/app/components/HousePoolTransaction/HousePoolTransaction';
 const HousePool = () => {
 
     const [showDepositModal, setshowDepositModal] = useState(false)
+    const [showDisclaimer, setshowDisclaimer] = useState(false)
     return (
         <HousePoolCont>
             <InfoContainer>
@@ -20,7 +23,7 @@ const HousePool = () => {
                         <H3>Pulse Token (PLS) </H3>
                         <p>Choose your odds and roll the dice to win pulse and prizes. Play, Invest, Exchange <br />
                             and Join the Contest with high rewards at Pulseroll</p>
-                        <Link>Read our disclaimer to know more</Link>
+                        <Link onClick={() => setshowDisclaimer(true)}>Read our disclaimer to know more</Link>
                     </FlexCont>
                     <FlexCont
                         justifyContent="center"
@@ -112,7 +115,11 @@ const HousePool = () => {
                     alignItems="center"
                 >Liquidity graph</FlexCont>
             </PoolDetailsContainer>
+            <TransactionContainer>
+                <h1>Transactions</h1>
+                <HousePoolTransaction />
 
+            </TransactionContainer>
 
             <CustomModal
                 show={showDepositModal}
@@ -121,6 +128,16 @@ const HousePool = () => {
             >
                 <HousePoolModal />
             </CustomModal>
+
+
+            <CustomModal
+                show={showDisclaimer}
+                heading="DISCLAIMER"
+                toggleModal={() => setshowDisclaimer(false)}
+            >
+                <Disclaimer />
+            </CustomModal>
+
         </HousePoolCont >
     );
 };
