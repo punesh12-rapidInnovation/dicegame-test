7867ea16576de08051d949b6262a4064db6ab8be
punesh12-rapidInnovation <punesh@rapidinnovation.dev>
2021-12-01 11:28:13 +0530

    increase coin size

--- src/modules/betting/modals/treasureChestStyle.ts
+++ src/modules/betting/modals/treasureChestStyle.ts
@@ -4,161 +4,161 @@ import { motion } from 'framer-motion';
 const example = keyframes`
 	0%   {
 			  top:0%; 
-			  transform: scale(0.1);
+			  transform: scale(1);
 			}
  	10%  {
 		  top:-20%; 
-		transform: scale(0.2)	;
+		transform: scale(1.2)	;
 					}
  	20%  { 
 		 top:-30%;
-		transform: scale(0.3);
+		transform: scale(1.4);
 
 		}
  	30%  { 
 		 top:-40%; 
-		transform: scale(0.4);
+		transform: scale(1.6);
 
 		}
  	40% { 
 		 top:-30%; 
-		transform: scale(0.5);
+		transform: scale(1.8);
 		}
  	50%  {
 		  top:-20%; 
-		transform: scale(0.6)	;
+		transform: scale(2)	;
 					}
  	60%  { 
 		 top:-10%;
-		transform: scale(0.8);
+		transform: scale(2.2);
 
 		}
  	70%  { 
 		 top:-20%; 
-		transform: scale(1);
+		transform: scale(2.4);
 
 		}
  	80% { 
 		 top:-30%; 
-		transform: scale(1.2);
+		transform: scale(2.6);
 		}
 		
 	90%   {
 		top:-40%; 
-		transform: scale(1.4);
+		transform: scale(2.8);
 	  }
 	  
 	100%   {
 		top:-50%; 
-		transform: scale(1.6);
+		transform: scale(3);
 	  }
 `
 
 const example2 = keyframes`
 0%   {
 			top:0%; left:25%;
-			transform: scale(0.1);
+			transform: scale(0.3);
 
 		}
  	10%  {
 		  top:-5%; 	left:20%;
-		  transform: scale(0.2);
+		  transform: scale(0.6);
 		}
  	20%  { 
 		 top:-10%; 	left:15%;
-		 transform: scale(0.3);
+		 transform: scale(0.9);
 
 
 		}
  	30%  { 
 		 top:-5%; 	left:10%;
-		 transform: scale(0.4);
+		 transform: scale(1.2);
 		}
  	40% { 
 		 top:0%; 	left:5%;
-		 transform: scale(0.5);
+		 transform: scale(1.5);
 
 		}
  	50% { 
 		 top:5%; 	left:0%;
-		 transform: scale(0.6);
+		 transform: scale(1.8);
 
 		}
  	60% { 
 		 top:10%; 	left:-5%;
-		 transform: scale(0.7);
+		 transform: scale(2.1);
 		}
  	70% { 
 		 top:15%; 	left:-5%;
-		 transform: scale(0.8);
+		 transform: scale(2.4);
 
 		}
  	80% { 
 		 top:20%; 	left:-5%;
-		 transform: scale(0.9);
+		 transform: scale(2.7);
 
 		}
  	90% { 
 		 top:25%; 	left:-5%;
-		 transform: scale(1);
+		 transform: scale(3);
 
 		}
  	100% { 
 		 top:30%; 	left:-5%;
-		 transform: scale(1.1);
+		 transform: scale(3.3);
 
 		}`
 
 const example3 = keyframes`
 0%   {
 	top:0%; left:70%;
-	transform: scale(0.1);
+	transform: scale(0.2);
 
 }
 10%  {
   top:-5%; 	left:75%;
-  transform: scale(0.2);
+  transform: scale(0.4);
 
 }
 20%  {
  top:-10%; 	left:80%;
- transform: scale(0.3);
+ transform: scale(0.6);
 }
 30%  {
  top:-5%; 	left:85%;
- transform: scale(0.4);
+ transform: scale(0.8);
 }
 40% {
  top:0%; 	left:90%;
- transform: scale(0.5);
+ transform: scale(1);
 }
 50% {
  top:5%; 	left:95%;
- transform: scale(0.6);
+ transform: scale(1.2);
 }
 60% {
  top:10%; 	left:95%;
- transform: scale(0.7);
+ transform: scale(1.4);
 
 }
 70% {
  top:15%; 	left:95%;
- transform: scale(0.8);
+ transform: scale(1.6);
 
 }
 80% {
  top:20%; 	left:95%;
- transform: scale(0.9);
+ transform: scale(1.8);
 
 }
 90% {
  top:25%; 	left:95%;
- transform: scale(1);
+ transform: scale(2);
 
 }
 100% {
  top:30%; 	left:95%;
- transform: scale(1.1);
+ transform: scale(2.2);
 
 }`
 
@@ -226,47 +226,47 @@ const example5 = keyframes`
 
 		}
 	  10%  {
-		  top:-5%; 	left:15%;
+		  top:-%; 	left:10%;
 		  transform: scale(1.1);
 
 		}
 	  20%  {
-		 top:-10%; 	left:10%;
+		 top:-10%; 	left:5%;
 		 transform: scale(1.2);
 		}
 	  30%  {
-		 top:-15%; 	left:5%;
+		 top:-15%; 	left:0%;
 		 transform: scale(1.3);
 		}
 	  40% {
-		 top:-20%; 	left:0%;
+		 top:-20%; 	left:-5%;
 		 transform: scale(1.4);
 		}
 	  50% {
-		 top:-15%; 	left:-5%;
+		 top:-15%; 	left:-10%;
 		 transform: scale(1.5);
 		}
 	  60% {
-		 top:-10%; 	left:-10%;
+		 top:-10%; 	left:-15%;
 		 transform: scale(1.3);
 		}
 	  70% {
-		 top:-5%; 	left:-15%;
+		 top:-5%; 	left:-20%;
 		 transform: scale(1.1);
 
 		}
 	  80% {
-		 top:0%; 	left:-15%;
+		 top:0%; 	left:-20%;
 		 transform: scale(0.9);
 
 		}
 	  90% {
-		 top:5%; 	left:-15%;
+		 top:5%; 	left:-20%;
 		 transform: scale(0.7);
 
 		}
 	  100% {
-		 top:10%; 	left:-15%;
+		 top:10%; 	left:-20%;
 		 transform: scale(0.5);
 
 		}
@@ -446,39 +446,38 @@ position: relative;
 
 .pulsecoin{
 	position: absolute;
-	top: 10 %;
-	left: 30 %;
+	top: 10%;
+	left: 30%;
 	height: 40px;
 	width: 40px;
 	animation: ${example} 1.5s infinite;
 }
 .pulsecoin2{
 	position: absolute;
-	top: 10 %;
-	left: 30 %;
+	top: 10%;
+	left: 30%;
 	height: 20px;
 	width: 20px;
 	animation: ${example2} infinite;
-	animation - duration: 2s;
+	animation-duration: 2s;
 }
 .pulsecoin3{
 	position: absolute;
-	top: 10 %;
-	left: 90 %;
+	top: 10%;
+	left: 90%;
 	height: 20px;
 	width: 20px;
 	animation: ${example3} infinite;
-	animation - duration: 2s;
+	animation-duration: 2s;
 }
 .pulsecoin4{
 	position: absolute;
-	top: 10 %;
-	left: 90 %;
+	top: 10%;
+	left: 90%;
 	height: 20px;
 	width: 20px;
 	animation: ${example4} infinite;
-
-	animation - duration: 4s;
+	animation-duration: 4s;
 }
 .pulsecoin5{
 	position: absolute;
@@ -487,7 +486,7 @@ position: relative;
 	height: 20px;
 	width: 20px;
 	animation: ${example5} infinite;
-	animation - duration: 4s;
+	animation-duration: 4s;
 }
 
 .pulsecoin6{
@@ -497,7 +496,7 @@ position: relative;
 	height: 20px;
 	width: 20px;
 	animation: ${example6} infinite;
-	animation - duration: 2s;
+	animation-duration: 2s;
 }
 .pulsecoin7{
 	position: absolute;
@@ -506,7 +505,7 @@ position: relative;
 	height: 20px;
 	width: 20px;
 	animation: ${example7} infinite;
-	animation - duration: 2s;
+	animation-duration: 2s;
 }
 
 .pulsecoin8{
@@ -516,7 +515,7 @@ position: relative;
 	height: 20px;
 	width: 20px;
 	animation: ${example8} infinite;
-	animation - duration: 4s;
+	animation-duration: 4s;
 }
 
 .pulsecoin9{
@@ -525,9 +524,9 @@ position: relative;
 	left: 30 %;
 	height: 20px;
 	width: 20px;
-	animation - name: example9;
-	animation - duration: 1.5s;
-	animation - iteration - count: infinite;
+	animation-name: example9;
+	animation-duration: 1.5s;
+	animation-iteration-count: infinite;
 }
 
 .pulsecoin10{
@@ -537,7 +536,5 @@ position: relative;
 	height: 20px;
 	width: 20px;
 	animation: ${example10} infinite;
-	animation - duration: 1s;
+	animation-duration: 1s;
 } `
-
-
