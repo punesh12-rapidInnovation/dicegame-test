6e315a7356ff1df08a9d1653181bfadc9136108b
punesh12-rapidInnovation <punesh@rapidinnovation.dev>
2021-12-21 11:50:44 +0530

    minor changes

--- src/shared/circleTimer/CircleTimer.tsx
+++ src/shared/circleTimer/CircleTimer.tsx
@@ -1,57 +1,57 @@
-import {useState, useEffect, memo} from "react";
+import { useState, useEffect, memo } from "react";
 import { TimerWrapper } from "modules/app/components/HousePoolTransaction/style";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 
 const CircleTimer = (props: any) => {
-    const { value, rowIndex } = props;
-    const lockedTime:any = localStorage.getItem(`lockedTime${rowIndex}`);
-    const [counter, setCounter] = useState(lockedTime);
-  
-    useEffect(() => {
-      
-      
-      if (!parseFloat(counter)) return;
-      
-      const intervalId = setInterval(() => {
-          // const lockedTimeString:any = localStorage.getItem(`lockedTime${rowIndex}`);
-          // let lockedTime = parseFloat(lockedTimeString);
-          console.log(counter);
-          // setLockedTimeLeft(newValue);
-          // const lockedTimeArrayString:any = localStorage.getItem("lockedTimeArray");
-          // let lockedTimeArray = JSON.parse(lockedTimeArrayString);
-          // localStorage.setItem(`lockedTime${rowIndex}`, `${counter - 1}`)
-          
-          setCounter(localStorage.getItem(`lockedTime${rowIndex}`));
-        }, 1000);
+  const { value, rowIndex } = props;
+  const lockedTime: any = localStorage.getItem(`lockedTime${rowIndex}`);
+  const [counter, setCounter] = useState(lockedTime);
 
-        return () => clearInterval(intervalId);
-    },[counter])
+  useEffect(() => {
 
-    // const renderTime = ({ remainingTime }:{remainingTime:any}) => {
-    //     //   console.log("value",value);
-    //     //   console.log("rendered");
-          
-    //     if (remainingTime === 0) {
-    //         return <div className="timer">0</div>;
-    //     }
-    //     else
-    //         return (
-    //             <div className="timer">
-    //                 {/* <div className="text">Remaining time</div> */}
-    //                 <div className="value">{formatRemainingTime(remainingTime)}</div>
-    //             </div>
-    //         );
-    // }
 
-    // const formatRemainingTime = (time: any) => {
-    //     const minutes = Math.floor((time % 3600) / 60);
-    //     const seconds = time % 60;
+    if (!parseFloat(counter)) return;
 
-    //     return `${minutes}:${seconds}`;
-    // };
-  
-    return (
+    const intervalId = setInterval(() => {
+      // const lockedTimeString:any = localStorage.getItem(`lockedTime${rowIndex}`);
+      // let lockedTime = parseFloat(lockedTimeString);
+      console.log(counter);
+      // setLockedTimeLeft(newValue);
+      // const lockedTimeArrayString:any = localStorage.getItem("lockedTimeArray");
+      // let lockedTimeArray = JSON.parse(lockedTimeArrayString);
+      // localStorage.setItem(`lockedTime${rowIndex}`, `${counter - 1}`)
+
+      setCounter(localStorage.getItem(`lockedTime${rowIndex}`));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [counter])
+
+  // const renderTime = ({ remainingTime }:{remainingTime:any}) => {
+  //     //   console.log("value",value);
+  //     //   console.log("rendered");
+
+  //     if (remainingTime === 0) {
+  //         return <div className="timer">0</div>;
+  //     }
+  //     else
+  //         return (
+  //             <div className="timer">
+  //                 {/* <div className="text">Remaining time</div> */}
+  //                 <div className="value">{formatRemainingTime(remainingTime)}</div>
+  //             </div>
+  //         );
+  // }
+
+  // const formatRemainingTime = (time: any) => {
+  //     const minutes = Math.floor((time % 3600) / 60);
+  //     const seconds = time % 60;
+
+  //     return `${minutes}:${seconds}`;
+  // };
+
+  return (
     //     <TimerWrapper >
     //     <CountdownCircleTimer
     //         isPlaying
@@ -67,10 +67,9 @@ const CircleTimer = (props: any) => {
     //         {renderTime}
     //     </CountdownCircleTimer>
     // </TimerWrapper>
-    <div style={{color:"#fff"}}>
-        {counter}
+    <div style={{ color: "#fff" }}>
+      {counter}
     </div>
-    );
-  };
-  export default memo(CircleTimer);
-  
+  );
+};
+export default memo(CircleTimer);
