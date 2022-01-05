c4f35846655d1a5aa9537811ed552a999ec07965
deepam <you@example.com>
2022-01-05 17:44:41 +0530

    sound check

--- src/modules/betting/Sound.js
+++ src/modules/betting/Sound.js
@@ -1,19 +1,19 @@
-import heart from '../../assets/sound/heartbeat.mp3';
+import heart from "../../assets/sound/heartbeat.mp3";
 // import win from 'assets/sound/bigWin.wav';
-import RollingDice from '../../assets/sound/Dice2.wav';
-import bigWin from '../../assets/sound/bigWin.wav';
-import bigLoose from '../../assets/sound/bigLoose.wav';
+import RollingDice from "../../assets/sound/Dice2.wav";
+import bigWin from "../../assets/sound/bigWin.wav";
+import bigLoose from "../../assets/sound/bigLoose.wav";
 
 export { heart, bigWin, bigLoose };
 
 export const Sound = (audioSource, play, loop) => {
-	const audio = new Audio(audioSource);
+  const audio = new Audio(audioSource);
 
-	audio.loop = loop;
+  audio.loop = loop;
 
-	if (play) {
-		audio.play();
-	}
+  if (play) {
+    audio.play();
+  }
 };
 
 // export const rollingDiceSound = new Audio(RollingDice);
@@ -21,34 +21,29 @@ export const rollingDiceSound = new Audio(heart);
 export const betWinSound = new Audio(bigWin);
 export const betLooseSound = new Audio(bigLoose);
 
-export const rangeSliderSound = (
-	playbackRate,
-	play,
-	soundFlag,
-	setSoundFlag
-) => {
-	try {
-		const audio = new Audio(heart);
+export const rangeSliderSound = (playbackRate, play, soundFlag, setSoundFlag) => {
+  try {
+    const audio = new Audio(heart);
 
-		audio.playbackRate = playbackRate;
-		audio.play();
-		// if (soundFlag === 0) {
-		// 	audio
-		// 		.play()
-		// 		.then(() => {
-		// 			// Audio is playing.
-		// 		})
-		// 		.catch((error) => {
-		// 			console.log(error);
-		// 		});
-		// 	setSoundFlag(1);
-		// }
+    audio.playbackRate = playbackRate;
+    audio.play();
+    // if (soundFlag === 0) {
+    // 	audio
+    // 		.play()
+    // 		.then(() => {
+    // 			// Audio is playing.
+    // 		})
+    // 		.catch((error) => {
+    // 			console.log(error);
+    // 		});
+    // 	setSoundFlag(1);
+    // }
 
-		// audio.addEventListener('ended', function () {
-		// 	// console.log('playbackRate', playbackRate);
-		// 	// audio.playbackRate = playbackRate;
-		// 	// audio.volume = playbackRate;
-		// 	audio.play();
-		// });
-	} catch (error) {}
+    // audio.addEventListener('ended', function () {
+    // 	// console.log('playbackRate', playbackRate);
+    // 	// audio.playbackRate = playbackRate;
+    // 	// audio.volume = playbackRate;
+    // 	audio.play();
+    // });
+  } catch (error) {}
 };
