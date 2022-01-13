c378ebdb5a640b4e2913acf2c9dad3b60288555c
punesh12-rapidInnovation <punesh@rapidinnovation.dev>
2022-01-13 14:02:53 +0530

    waitingModal

--- src/modules/betting/modals/WaitingModal.tsx
+++ src/modules/betting/modals/WaitingModal.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import useSound from "use-sound";
 import { heart, rollingDiceSound } from '../Sound';
 
+import Hear from 'assets/sound/HumanHeart.mp3'
+
 
 import {
     ModalBody,
@@ -16,43 +18,47 @@ import {
 const WaitingModal = (props: any) => {
     const { show, toggleModal, styles } = props;
 
+    const rollingDiceSound = new Audio(Hear);
+    rollingDiceSound.preload = 'metadata'
 
-    const [play, { stop }] = useSound(heart, { interrupt: true });
+    // const [play, { stop }] = useSound(heart, { interrupt: true });
 
-    const handleClickOutside = (e: any) => {
-        if (e.target === e.currentTarget) {
-            toggleModal();
-        }
-    };
-
+    // const handleClickOutside = (e: any) => {
+    //     if (e.target === e.currentTarget) {
+    //         toggleModal();
+    //     }
+    // };
 
     useEffect(() => {
-        try {
-            const soundOff = localStorage.getItem("soundOff") || "";
-            if (soundOff !== 'true') {
+        const timer = setTimeout(() => {
+            try {
+                const soundOff = localStorage.getItem("soundOff") || "";
+                if (soundOff !== 'true') {
 
-                if (show) {
-                    // rollingDiceSound.play();
-                    // rollingDiceSound.loop = true;
-                    play();
-                }
-                else {
-                    // rollingDiceSound.loop = false;
-                    // rollingDiceSound.removeEventListener('ended', () => {
-                    //     rollingDiceSound.pause();
-                    //     rollingDiceSound.currentTime = 0;
-                    //     rollingDiceSound.src = ""
-                    // });
+                    if (show) {
+                        rollingDiceSound.play();
+                        rollingDiceSound.loop = true;
+                        // play();
 
-                    stop();
+                    }
+                    else {
+                        rollingDiceSound.loop = false;
+                        rollingDiceSound.removeEventListener('ended', () => {
+                            rollingDiceSound.pause();
+                            rollingDiceSound.currentTime = 0;
+                            rollingDiceSound.src = ""
+                        });
+                        // stop();
+                    }
                 }
+
+            } catch (error) {
+                console.log(error);
             }
+        }, 10);
+        return () => clearTimeout(timer);
+    }, [show]);
 
-        } catch (error) {
-            console.log(error);
-
-        }
-    })
 
     return (
         <ModalBody
