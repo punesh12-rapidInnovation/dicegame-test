32b33c550eccce63f4eb2e91676ed2d0b9c0c535
punesh12-rapidInnovation <punesh@rapidinnovation.dev>
2022-04-22 18:37:23 +0530

    address change

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,8 @@
 export const walletTestConnectId = "https://rpc-mumbai.matic.today";
 export const walletMainConnectId = "https://polygon-rpc.com/";
 
-export const API_URL= "https://pulseroll-api.pulseluck.com/pool";
+export const API_URL = "https://pulseroll-api.pulseluck.com/pool";
 export const SOCKET_URL = "https://pulseroll-subs.pulseluck.com";
-
 
 export const networkTestChainId = 80001;
 export const networkMainChainId = 137;
@@ -14,5 +13,7 @@ export const LINK_TOKEN_ADDRESS = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
 export const FACTORY_ADDRESS = "";
 
 export const PULSE_TOKEN_ADDRESS = "";
-export const BETTING_ADDRESS = "0xb8a122E5860B729CAc11eA53Ba1A8D48B607E7d5";
-export const HOUSEPOOL_ADDRESS = "0xCB19F9826a4485eF984027265bC8bDB9A0d6aB87";
+// export const BETTING_ADDRESS = "0xb8a122E5860B729CAc11eA53Ba1A8D48B607E7d5";
+export const BETTING_ADDRESS = "0x0Cd4F175Cb962C7ae9084dc165c0F2Eb244cC7A8";
+// export const HOUSEPOOL_ADDRESS = "0xCB19F9826a4485eF984027265bC8bDB9A0d6aB87";
+export const HOUSEPOOL_ADDRESS = "0xc105F83fB73E486aB6C44d1c21D1f6B3b28586ca";
